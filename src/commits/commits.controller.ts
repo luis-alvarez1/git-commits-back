c66273b8d38a1c4b337d44be2669eb2959c54a14
Luis Alvarez <lealvarezp@unac.edu.co>
2023-11-13 16:39:29 -0300

    changing methods to post

--- src/commits/commits.controller.ts
+++ src/commits/commits.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Param, Post } from '@nestjs/common';
 import { CommitsService } from './commits.service';
 import { FindCommitsDto } from './dto/find-all-commits.dto';
 
@@ -6,12 +6,12 @@ import { FindCommitsDto } from './dto/find-all-commits.dto';
 export class CommitsController {
   constructor(private readonly commitsService: CommitsService) {}
 
-  @Get()
+  @Post()
   findAll(@Body() dto: FindCommitsDto) {
     return this.commitsService.findAll(dto);
   }
 
-  @Get(':id')
+  @Post(':id')
   findOne(@Param('id') id: string, @Body() dto: FindCommitsDto) {
     return this.commitsService.findOne(id, dto);
   }

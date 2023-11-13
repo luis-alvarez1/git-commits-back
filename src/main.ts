eba87bdaaac7e4dc0b7f7e0e850f6d8a3f0b6ce9
Luis Alvarez <lealvarezp@unac.edu.co>
2023-11-13 15:14:45 -0300

    still struggling with cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -11,6 +11,8 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  app.enableCors();
 
   await app.listen(3000);
 }

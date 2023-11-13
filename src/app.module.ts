572d8ffaed86e384d34df73d63f29be8076e65af
Luis Alvarez <lealvarezp@unac.edu.co>
2023-11-13 10:42:31 -0300

    clean up and deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 import { CommitsModule } from './commits/commits.module';
 
 @Module({
   imports: [CommitsModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}

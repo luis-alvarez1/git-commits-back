5648d1968336c30775bbc37fe050a6aa75a5017b
Luis Alvarez <lealvarezp@unac.edu.co>
2023-11-13 16:45:45 -0300

    status codes

--- src/commits/commits.controller.ts
+++ src/commits/commits.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Param, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Param, Post } from '@nestjs/common';
 import { CommitsService } from './commits.service';
 import { FindCommitsDto } from './dto/find-all-commits.dto';
 
@@ -7,11 +7,13 @@ export class CommitsController {
   constructor(private readonly commitsService: CommitsService) {}
 
   @Post()
+  @HttpCode(200)
   findAll(@Body() dto: FindCommitsDto) {
     return this.commitsService.findAll(dto);
   }
 
   @Post(':id')
+  @HttpCode(200)
   findOne(@Param('id') id: string, @Body() dto: FindCommitsDto) {
     return this.commitsService.findOne(id, dto);
   }
